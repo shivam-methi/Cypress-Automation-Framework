1d6b11526709496a04b99702700e141712f43626
shivam-methi <methism26@gmail.com>
2024-11-28 16:31:35 +0530

    .

--- cypress/e2e/COMMANDS/12.mouse-actions-scrollIntoView.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("Test Mouse Actions", () => {
-
-    it("Scroll element into view", () => {
-        cy.visit("https://www.webdriveruniversity.com/");
-        cy.get('#actions').scrollIntoView().invoke('removeAttr', 'target').click({ force: true });
-    })
-})
